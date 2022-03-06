e4528dc231ee55af46298e6627404235ca24b389
Youssef Er-raki <yer-raki@e1r1p7.1337.ma>
2022-03-06 19:51:54 +0100

    starting random_access_iterator

---
+++ random_access_iterator.hpp
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   random_access_iterator.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yer-raki <yer-raki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/06 19:14:00 by yer-raki          #+#    #+#             */
+/*   Updated: 2022/03/06 19:50:19 by yer-raki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+
+namespace ft
+{
+	template <class Category, class T, class Distance = ptrdiff_t,
+        	class Pointer = T*, class Reference = T&>
+	class iterator {
+		public:
+			typedef T         value_type;
+			typedef Distance  difference_type;
+			typedef Pointer   pointer;
+			typedef Reference reference;
+			typedef Category  iterator_category;
+	};
+	
+	template <typename T>
+	class random_access_iterator : ft::iterator<std::random_access_iterator_tag, T>
+	{
+		public:
+        	typedef typename ft::iterator<std::random_access_iterator_tag, T>::iterator_category	iterator_category;
+        	typedef typename ft::iterator<std::random_access_iterator_tag, T>::difference_type		difference_type
+        	typedef typename ft::iterator<std::random_access_iterator_tag, T>::value_type			value_type;
+			typedef	T*																				pointer;
+			typedef	T&																				reference;
+		
+		public:
+			random_access_iterator() : _ptr(nullptr);
+			~random_access_iterator();
+			random_access_iterator(random_access_iterator const & src) { return (*this = src); }
+			random_access_iterator & operator=(random_access_iterator const & src)
+			{
+				if (this != &src)
+					_ptr = src._ptr;
+				return (*this);
+			}
+			bool operator==(random_access_iterator const & src) { return (_ptr == src._ptr); }
+			bool operator!=(random_access_iterator const & src) { return (_ptr != src._ptr); }
+			random_access_iterator & operator++()
+			{
+				_ptr++;
+				return (*this);
+			}
+			random_access_iterator operator++(int)
+			{
+				random_access_iterator tmp = *this;
+				operator++();
+				return (tmp);
+			}
+			random_access_iterator & operator--()
+			{
+				_ptr--;
+				return (*this);
+			}
+			random_access_iterator operator--(int)
+			{
+				random_access_iterator tmp = *this;
+				operator--();
+				return (tmp);
+			}
+			// random_access_iterator * operator*()
+			// {
+			// 	return (_ptr);
+			// }
+			T & operator*()
+			{
+				return (*_ptr);
+			}
+			T * operator->()
+			{
+				return (_ptr);
+			}
+
+		private:
+			T	*_ptr;
+	};
+	
+}
